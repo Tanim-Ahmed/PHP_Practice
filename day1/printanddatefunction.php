a15d4c9f0ef92463e5924a96662c5f9e7849d7e9
Tanim Ahmed <tanim.ahmed.oct@gmail.com>
2021-06-16 17:50:52 +0600

    Uploaded by Tanim Ahmed

---
+++ day1/printanddatefunction.php
@@ -0,0 +1,13 @@
+<?php
+//print something
+echo "Tanim Ahmed"."<br>";
+echo "Hello World"."<br>";
+//use of date function
+echo "Today's date:".date("d/m/Y")."<br>";
+
+echo "Today's date is:".date("d.m.Y")."<br>";
+
+echo "Today is:".date("l")."<br>";
+echo "Time:".date("h:i:sa")."<br>";
+echo "Year:".date("Y")."<br>";
+?>
